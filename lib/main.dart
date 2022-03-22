2706343373d873dc29a067944de35a7323eb6cd1
djinodissingar <dissingardjino@icloud.com>
2022-03-22 16:31:50 +0100

    first Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled1/model/Musique.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,18 +49,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  //Variable
+  List<Musique> allMorceau = [
+    Musique(path: "assets/sons/14.Alibi.mp3", title: "Bonjour", artist: "Melanié",image:"assets/images/1d84dc7609a129e377cc38afdf38b9d064183853.jpg"),
+    Musique(path: "assets/sons/13.GoodnightJacks.mp3", title: "Big Boss", artist: "Djino",genre: ["rap"]),
+    Musique(path: "assets/sons/14.Alibi.mp3", title: "Love tender", artist: "Dune",image: "assets/images/07.jpg"),
+    Musique(path: "assets/sons/18.LetPoirotWork.mp3", title: "Hymn", artist: "Léa",image: "assets/images/36morning_banana_oatmeal.jpg"),
+
+  ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -75,41 +74,32 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: bodyPage(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      backgroundColor: Colors.amber,
+
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget bodyPage(){
+    return GridView.builder(
+      itemCount: allMorceau.length,
+        gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,crossAxisSpacing: 10,mainAxisSpacing:20),
+      itemBuilder: (context,index){
+          return Container(
+            height: 250,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                image: (allMorceau[index].image ==  null)?AssetImage("assets/images/indispo.jpg"):AssetImage(allMorceau[index].image!),
+                fit: BoxFit.fill
+              )
+            ),
+          );
+      },
     );
   }
 }
